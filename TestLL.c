d363887a8a10bdb918d2248b0cca345966c3a82b
Yared K Estifanos <Yared.Estifanos@gmail.com>
2023-11-10 15:12:01 -0600

    Create TestLL.c
    LinkedList LL and some other ...

---
+++ TestLL.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node {
+    int data;
+    struct Node* next;
+} LL;
+
+void add(LL** head, int _data) {
+    LL* newNode = (LL*)malloc(sizeof(LL));
+    newNode->data = _data;
+    newNode->next = NULL;
+
+    if (*head == NULL) {
+        *head = newNode;
+    } else {
+        LL* current = *head;
+        while (current->next != NULL) {
+            current = current->next;
+        }
+        current->next = newNode;
+    }
+}
+void printList(LL* head) {
+    LL* current = head;
+    while (current != NULL) {
+        printf("%d ", current->data);
+        current = current->next;
+    }
+    printf("\n");
+}
+void convertToBinary(int n) {
+    // binary format
+    int r = n;
+
+    while (r) {
+        printf("%d", r % 2);
+        r = r >> 1;
+    }
+}
+int main() {
+    LL* head = NULL;
+    for (int inx = 0; inx < 5; inx++) {
+        add(&head, inx);
+        printList(head);
+    }
+
+    // Clean up memory
+    LL* current = head;
+    LL* next;
+    while (current != NULL) {
+        next = current->next;
+        free(current);
+        current = next;
+    }
+    return 0;
+}
